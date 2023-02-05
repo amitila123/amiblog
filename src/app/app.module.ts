ac7c1572a5de497cc82db7e18a7af2d7052810a6
amitila <chuatam80@gmail.com>
2023-02-05 11:45:21 +0700

    update home, profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MyProfileComponent,
+    HomeComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
